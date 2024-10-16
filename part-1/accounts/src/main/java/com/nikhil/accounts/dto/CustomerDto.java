246485017756b7b81edbeeec74f2805aaecc69d7
Nikhil27-web <nikhilshankarsg@gmail.com>
2024-10-17 00:22:58 +0530

    Swagger Documentation for Accounts Added
    Build Microservices of Accounts

--- part-1/accounts/src/main/java/com/nikhil/accounts/dto/CustomerDto.java
+++ part-1/accounts/src/main/java/com/nikhil/accounts/dto/CustomerDto.java
@@ -1,5 +1,6 @@
 package com.nikhil.accounts.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
@@ -8,18 +9,34 @@ import lombok.Data;
 
 
 @Data
+@Schema(
+        name = "Customer",
+        description = "Schema to hold Customer and Account information"
+)
 public class CustomerDto {
 
     @NotEmpty(message = "Name can not be a null or empty")
     @Size(min=5, max=30, message="The length of the customer name between 5 and 30" )
+    @Schema(
+            description = "Name of the customer", example = "Nikhil Shankar S G"
+    )
     private String name;
 
     @NotEmpty(message = "Email address can not be a null or empty")
     @Email(message = "Email Should be a valid value")
+    @Schema(
+            description = "Email address of the customer", example = "nikhilshankarsg@gmail.com"
+    )
     private String email;
 
     @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits")
+    @Schema(
+            description = "Mobile Number of the customer", example = "9739701296"
+    )
     private String mobileNumber;
 
+    @Schema(
+            description = "Account details of the Customer"
+    )
     private AccountsDto accountsDto;
 }
--- part-1/accounts/src/main/java/com/nikhil/accounts/dto/ErrorResponseDto.java
+++ part-1/accounts/src/main/java/com/nikhil/accounts/dto/ErrorResponseDto.java
@@ -1,5 +1,6 @@
 package com.nikhil.accounts.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.http.HttpStatus;
@@ -7,14 +8,30 @@ import org.springframework.http.HttpStatus;
 import java.time.LocalDateTime;
 
 @Data @AllArgsConstructor
+@Schema(
+        name = "ErrorResponse",
+        description = "Schema to hold error response information"
+)
 public class ErrorResponseDto {
 
+    @Schema(
+            description = "API path invoked by client"
+    )
     public String apiPath;
 
+    @Schema(
+            description = "Error code representing the error happened"
+    )
     public HttpStatus errorCode;
 
+    @Schema(
+            description = "Error message representing the error happened"
+    )
     public String errorMsg;
 
+    @Schema(
+            description = "Time representing when the error happened"
+    )
     public LocalDateTime errorTime;
 
 }
